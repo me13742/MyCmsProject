0f9066a4563c0c5103a0413967bf8cc0a712ec47
Amir <me137420000@gmail.com>
2021-09-29 16:35:14 +0330

    Add Admin Area v 1.0

---
+++ MyCms/Areas/Admin/Controllers/PageGroupsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MyCms.Areas.Admin.Controllers
+{
+    public class PageGroupsController : Controller
+    {
+        private MyCmsContext db = new MyCmsContext();
+
+        // GET: Admin/PageGroups
+        public ActionResult Index()
+        {
+            return View(db.PageGroups.ToList());
+        }
+
+        // GET: Admin/PageGroups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = db.PageGroups.Find(id);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PageGroups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PageGroups.Add(pageGroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = db.PageGroups.Find(id);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pageGroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = db.PageGroups.Find(id);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PageGroup pageGroup = db.PageGroups.Find(id);
+            db.PageGroups.Remove(pageGroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
